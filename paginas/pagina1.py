06e3074248fa0db22f3d9bcc909c1662eb838f4a
Ricardo Rocha <ricardo8610@gmail.com>
2025-08-01 11:49:43 -0300

    erro

--- paginas/pagina1.py
+++ paginas/pagina1.py
@@ -26,7 +26,7 @@ if st.button("Criar"):
     informacoes = {'nome': st.user.name,
                     'foto': st.user.picture,
                     'email': st.user.email,
-                    'hora': datetime.now()strftime("%H:%M:%S")}
+                    'hora': datetime.now().strftime("%H:%M:%S")}
     db.collection(colecao).add(informacoes)
     st.write(f"Criado com ID: {st.user.email}")
 
