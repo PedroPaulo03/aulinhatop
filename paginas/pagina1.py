8d75da888cd6433f21af5e3fac9a77df15ca2101
PedroPaulo03 <pedropaulog333@gmail.com>
2025-08-01 12:05:35 -0300

    kjhkjk

--- paginas/pagina1.py
+++ paginas/pagina1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
-from datetimetime import datetime
+from datetime import datetime
 st.title("Exemplos CRUD - Firebase")
 
 # Conectar Firebase
