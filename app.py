adb44cd2961346bd08342bcbd73ed33bb2f690e5
PedroPaulo03 <pedropaulog333@gmail.com>
2025-08-15 11:07:10 -0300

    7

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import streamlit as st 
 
 paginas = {
-    "Página de acesso": [ st.Page("pagina/inicial.py", title="Início", default=True)],
+    "Páginas": [ st.Page("paginas/inicial.py", title="Início", icon='🚓', default=True)],
     
-    "Transformações": [ st.Page("paginas/pagina1.py", title="Escrivão LaTeX"), 
-           ]
+    "Exemplo": [
+        st.Page("paginas/pagina1.py", title="Banco de Dados", icon='🚙'),
+        st.Page("paginas/gamificacao.py", title="Gamificação", icon='🎯')
+        # st.Page("paginas/pagina2.py", title="Exemplo", icon='⚡')
+    ]
 }
 
 # Usa a estrutura de páginas final (com ou sem Admin)
 pg = st.navigation(paginas)
 pg.run()
-
 
