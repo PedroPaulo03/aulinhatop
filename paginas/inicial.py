e5562fcfec6afc66c587ef0ef1d2540981288ec3
PedroPaulo03 <pedropaulog333@gmail.com>
2025-08-19 11:23:38 -0300

    8

--- paginas/inicial.py
+++ paginas/inicial.py
@@ -7,16 +7,7 @@ if not st.user.is_logged_in:
     st.title("FotoLateX")
     st.markdown("*Bem-vindo ao seu escrivão especializado! Aqui você pode acompanhar seus códigos em LateX.*")
     st.markdown("É necessário log in para o uso da ferramenta, clique no botão a seguir. ")
-    # Logo centralizada
-        st.image('arquivos/capa.jpg', width=200, use_container_width=True)
-        # Botão de login
-        if st.button("Login com Google", type="primary", use_container_width=True, icon=':material/login:'):
-            # Registra o usuário no Firestore se for o primeiro acesso (login_usuario faz isso)
-            # REMOVIDO DAQUI: login_usuario() 
-            st.login() # Função de login do Streamlit (redireciona)
     # st.write(st.user)
     if st.button("Log in"):
         st.login()
 
-if st.sidebar.button("Log out"):
-    st.logout()
