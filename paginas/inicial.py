eca862d7fadf8b4435016799c3ce38a523c27827
PedroPaulo03 <pedropaulog333@gmail.com>
2025-08-19 12:11:40 -0300

    88

--- paginas/inicial.py
+++ paginas/inicial.py
@@ -9,8 +9,9 @@ if not st.user.is_logged_in:
     st.image('paginas/arquivos/imagelatex.png', width=200, use_container_width=True)
     st.markdown("Faça login com sua conta Google para o uso da ferramenta. ")
     # st.write(st.user)
-    if st.button("Log in"):
-        st.login()
-
+    if st.button("Login com Google", type="primary", use_container_width=True, icon=':material/login:'):
+            # Registra o usuário no Firestore se for o primeiro acesso (login_usuario faz isso)
+            # REMOVIDO DAQUI: login_usuario() 
+            st.login() # Função de login do Streamlit (redireciona)
 if st.sidebar.button("Log out"):
     st.logout()
