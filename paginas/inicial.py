e39a0de523544e6f8477024027aec1baff223253
PedroPaulo03 <pedropaulog333@gmail.com>
2025-08-19 11:43:07 -0300

    8

--- paginas/inicial.py
+++ paginas/inicial.py
@@ -6,7 +6,8 @@ from google.genai import types
 if not st.user.is_logged_in:
     st.title("FotoLateX")
     st.markdown("*Bem-vindo ao seu escrivão especializado! Aqui você pode acompanhar seus códigos em LateX.*")
-    st.markdown("É necessário log in para o uso da ferramenta, clique no botão a seguir. ")
+    st.image('arquivos/capa.jpg', width=200, use_container_width=True)
+    st.markdown("Faça login com sua conta Google para o uso da ferramenta. ")
     # st.write(st.user)
     if st.button("Log in"):
         st.login()
