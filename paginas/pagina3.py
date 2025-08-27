f8f1149171f00b31f203c1020154d06c24d2f5d2
PedroPaulo03 <pedropaulog333@gmail.com>
2025-08-27 12:44:26 -0300

    erros

--- paginas/pagina3.py
+++ paginas/pagina3.py
@@ -12,35 +12,36 @@ if st.user:
     colecao = 'usuarios2'
     referencia = db.collection(colecao).document(st.user.email).collection('saidas')
 
-    if saidas:
-        docs = referencia.stream()
+    # Pega todos os documentos da subcoleção
+    docs = list(referencia.stream())
+
+    if docs:  # verifica se a lista de documentos não está vazia
         lista_ids_saidas = []
         for doc in docs:
-            id = doc.id
+            doc_id = doc.id
             try:
-                 # Converte string para datetime
-                dt = datetime(id, "%Y%m%d%H%M%S")
+                # Converte string para datetime
+                dt = datetime.strptime(doc_id, "%Y%m%d%H%M%S")
                 # Formata para string legível
                 legivel = dt.strftime("%d/%m/%Y %H:%M:%S")
                 lista_ids_saidas.append(f'Feito em: {legivel}')
-
             except Exception as e:
                 print(f'Erro ao transformar a data em formato legível: {e}')
-            
-        visualizacao = st.selectbox('Selecione uma saída para visualizar', options = lista_ids_saidas)
+
+        visualizacao_index = st.selectbox('Selecione uma saída para visualizar', options=range(len(lista_ids_saidas)),
+                                          format_func=lambda i: lista_ids_saidas[i])
 
         if st.button('Visualizar'):
+            doc_selecionado = docs[visualizacao_index].to_dict()
             col1, col2 = st.columns(2)
-            documento = db.collection(colecao).document(st.user.email).collection('saidas').document(visualizacao).get().to_dict()
-             
             with col1:
-                 st.header('Código LaTeX')
-                 codigo_latex = documento.get("saida_latex")
-                 st.code(codigo_latex)
+                st.header('Código LaTeX')
+                codigo_latex = doc_selecionado.get("saida_latex")
+                st.code(codigo_latex)
             with col2:
-                 st.header('Código Markdown')
-                 codigo_markdown = documento.get("saida_markdown")
-                 st.code(codigo_markdown)
+                st.header('Código Markdown')
+                codigo_markdown = doc_selecionado.get("saida_markdown")
+                st.code(codigo_markdown)
     else:
         st.info('Você ainda não carregou nenhuma imagem. Acesse a aba Transformação para converter sua primeira imagem em código LaTeX ou Markdown!')
 else:
