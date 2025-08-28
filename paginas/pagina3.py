9e092ae0543413ad1d6637ff14e8251e86088ba1
PedroPaulo03 <pedropaulog333@gmail.com>
2025-08-28 11:15:40 -0300

    55

--- paginas/pagina3.py
+++ paginas/pagina3.py
@@ -33,29 +33,30 @@ if st.user:
         visualizacao = st.selectbox(
         'Selecione uma saída para visualizar',
         options=lista_ids_saidas if lista_ids_saidas else ["Nenhuma saída disponível"]
-            )
-
-    if visualizacao != "Nenhuma saída disponível":
-    # Em vez de usar index(), use dict para mapear
-    mapa_saidas = dict(zip(lista_ids_saidas, lista_docs_ids))
-    doc_id_real = mapa_saidas[visualizacao]
-
-    if st.button('Visualizar'):
-        col1, col2 = st.columns(2)
-        documento = (
-            db.collection(colecao)
-              .document(st.user.email)
-              .collection('saidas')
-              .document(doc_id_real)
-              .get()
-              .to_dict()
         )
 
-        with col1:
+        if visualizacao != "Nenhuma saída disponível":
+        # Em vez de usar index(), use dict para mapear
+        mapa_saidas = dict(zip(lista_ids_saidas, lista_docs_ids))
+        doc_id_real = mapa_saidas[visualizacao]
+
+            if st.button('Visualizar'):
+                col1, col2 = st.columns(2)
+                documento = (
+                db.collection(colecao)
+                    .document(st.user.email)
+                    .collection('saidas')
+                    .document(doc_id_real)
+                    .get()
+                    .to_dict()
+        )
+
+            with col1:
             st.header('Código LaTeX')
             codigo_latex = documento.get("saida_latex", "Não encontrado")
             st.code(codigo_latex)
-        with col2:
+            
+            with col2:
             st.header('Código Markdown')
             codigo_markdown = documento.get("saida_markdown", "Não encontrado")
             st.code(codigo_markdown)
